87529f68240814b1a1f9299ef117f873508ae17b
Guillaume Devillers <guillaume.devillers@de.ibm.com>
2023-03-16 21:50:54 +0100

    case parameter

--- addUserToCaseWatchlist.sh
+++ addUserToCaseWatchlist.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-stdout=/tmp/stdout
-stderr=/tmp/stderr
-export cases_json=/tmp/cases.json
-loglevel=5
+export stdout=/tmp/stdout
+export stderr=/tmp/stderr
+export json=/tmp/json
+export loglevel=5
+export caseParameters=""
+
+[ -f ${json} ] && rm ${json}
+[ -f ${stderr} ] && rm ${stderr}
+[ -f ${stdout} ] && rm ${stdout}
 
 usage() {
-  info "USAGE: $0 [-a|--apikey APIKEY] [[-e|--email IBM_CLOUD_USER_ID]] [-d|--debug]"
+  info "USAGE: $0 [-a|--apikey APIKEY] [[-e|--email IBM_CLOUD_USER_ID]] [[-c|--case IBM_CLOUD_SUPPORT_CASE]] [-d|--debug]"
   exit 1
 }
 
@@ -30,30 +35,67 @@ debug() {
   [ ${loglevel} -ge 10 ] && log DEBUG $* 
 }
 
-while test $# -gt 0
-do
-  parameter=$1
-  shift
-  case $parameter in
-    -a|--apikey)
-      apikey=$1
-      shift
-      ;;
-    -d|--debug)
-      loglevel=10
-      ;;
-    -e|--email)
-      emails="${emails} $1"
-      shift
-      ;;
-    *)
-    usage
-  esac
-done
+getparameters() {
+  while test $# -gt 0
+  do
+    parameter=$1
+    shift
+    case $parameter in
+      -a|--apikey)
+        apikey=$1
+        shift
+        debug API Key=${apykey}
+        ;;
+      -d|--debug)
+        loglevel=10
+        info Log Level=${loglevel}
+        ;;
+      -e|--email)
+        emailParameters="${emailParameters} $1"
+        shift
+        debug Email List=${emailParameters}
+        ;;
+      -c|--case)
+        caseParameters="${caseParameters} $1"
+        shift
+        debug Case List=${caseParameters}
+        ;;
+      *)
+        usage
+    esac
+  done
+
+  export emailParameters
+  export caseParameters
+}
+
+addusertocase() {
+  email=$1
+  case=$2
+  debug ADD IBM CLOUD USER ID ${email} TO CASE ${case} WATCHLIST
+  watchlist='{"watchlist":[{"realm":"IBMid","user_id":"'${email}'","type":"customer"}]}'
+  curl -X PUT https://support-center.cloud.ibm.com/case-management/v1/cases/${case}/watchlist --header 'Content-Type:application/json' -H Authorization:${token} -d ${watchlist} 1>${stdout} 2>${stderr}
+  if [ $? -ne 0 ]
+  then
+    debug $(cat ${stdout})
+    debug $(cat ${stderr})
+    warn FAILED TO ADD IBM CLOUD USER ID ${email} TO CASE ${case} WATCHLIST
+  else
+    cat ${stdout} | grep '"added": \[\],' >/dev/null
+    if [ $? -eq 0 ] 
+    then 
+      warn $(cat ${stdout})
+      warn FAILED TO ADD IBM CLOUD USER ID ${email} TO CASE ${case} WATCHLIST
+    else
+      info ADDED IBM CLOUD USER ID ${email} TO CASE ${case} WATCHLIST
+    fi
+  fi
+}
 
 addusertocases()
 { 
   email=$1
+
   debug ADD IBM CLOUD USER ID ${email} TO CASE WATCHLIST
 
   allCases=""
@@ -75,14 +117,14 @@ addusertocases()
       *)
         debug IGNORED LINE: $line
     esac
-  done <${cases_json}
+  done <${json}
   
-  debug ... ALL CASES = ${allCases}
-  debug ... EMAIL CASES = ${emailCases}
+  debug ALL CASES = ${allCases}
+  debug EMAIL CASES = ${emailCases}
 
   if test "${allCases}" = "" 
   then
-    debug ... NO CASES
+    warn NO ACTIVE CASES
     exit 0
   fi
 
@@ -91,65 +133,103 @@ addusertocases()
     echo "${emailCases}" | grep "${case}">/dev/null
     if test $? -eq 1
     then 
-      debug ADD IBM CLOUD USER ID ${email} TO CASE ${case} WATCHLIST
-      watchlist='{"watchlist":[{"realm":"IBMid","user_id":"'${email}'","type":"customer"}]}'
-      curl -X PUT https://support-center.cloud.ibm.com/case-management/v1/cases/${case}/watchlist --header 'Content-Type:application/json' -H Authorization:${token} -d ${watchlist} 1>${stdout} 2>${stderr}
-      if [ $? -ne 0 ]
-      then
-        debug $(cat ${stdout})
-        debuc $(cat ${stderr})
-        warn FAILED TO ADD IBM CLOUD USER ID ${email} TO CASE ${case} WATCHLIST
-      else
-        cat ${stdout} | grep '"added": \[\],' >/dev/null
-        if [ $? -eq 0 ] 
-        then 
-          warn $(cat ${stdout})
-          warn FAILED TO ADD IBM CLOUD USER ID ${email} TO CASE ${case} WATCHLIST
-        else
-          info ADDED IBM CLOUD USER ID ${email} TO CASE ${case} WATCHLIST
-        fi
-      fi
+      addusertocase $email $case
     else
       info IBM CLOUD USER ID ${email} ALREADY PART OF CASE ${case} WATCHLIST
     fi
   done
 }
 
-info GET IBM CLOUD IDENTITY TOKEN
-curl -X POST "https://iam.cloud.ibm.com/identity/token" --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' --data-urlencode grant_type=urn:ibm:params:oauth:grant-type:apikey --data-urlencode apikey=${apikey} 1>${stdout} 2>${stderr}
-if [ $?  -ne 0 ] 
+gettoken() {
+  info GET IBM CLOUD IDENTITY TOKEN
+  curl -X POST "https://iam.cloud.ibm.com/identity/token" --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' --data-urlencode grant_type=urn:ibm:params:oauth:grant-type:apikey --data-urlencode apikey=${apikey} 1>${stdout} 2>${stderr}
+  if [ $?  -ne 0 ] 
+  then
+    error $(cat ${stdout})
+    error $(cat ${stderr})
+    error ERROR GETTING IBM CLOUD IDENTITY TOKEN
+    exit 1
+  fi
+  export token=$(cat ${stdout} | grep -o '"access_token":"[^"]*' | grep -o '[^"]*$')
+  if [ "${token}" = "" ]
+  then
+    error $(cat ${stdout})
+    error FAILED TO GET IBM CLOUD IDENTITY TOKEN 
+    exit 1
+  fi
+}
+
+getallcases() {
+  info GET ALL IBM CLOUD CASES
+  curl -s -X GET "https://support-center.cloud.ibm.com/case-management/v1/cases?fields=number,watchlist&status=new,in_progress,waiting_on_client,resolution_provided" -H Authorization:${token} 1>${json} 2>${stderr}
+  if [ $? -ne 0 ] 
+  then
+    error $(cat ${json})
+    error $(cat ${stderr})
+    error ERROR GETTING IBM CLOUD CASES
+    exit 1
+  fi
+
+  cat ${json} | grep '"number":' >/dev/null
+  if  [ $? -ne 0 ]
+  then
+    debug $(cat ${debug})
+    warn NO IBM CLOUD CASES OPENED
+  fi
+}
+
+getcases() {
+  info "GET CASE(S)" $*
+  for case in $*
+  do
+    getcase ${case}
+  done
+}
+
+getcase() {
+  info GET IBM CLOUD CASE $1
+  case=$1
+  curl -s -X GET "https://support-center.cloud.ibm.com/case-management/v1/cases/${case}?fields=number,watchlist" -H Authorization:${token} 1>${stdout} 2>${stderr}
+  if [ $? -ne 0 ] 
+  then
+    error $(cat ${stdout})
+    error $(cat ${stderr})
+    error ERROR GETTING IBM CLOUD CASE ${case}
+    exit 1
+  else
+    cat ${stdout} | grep "not_found" >/dev/null
+    if [ $? -eq 0 ]
+    then
+      warn $(cat ${stdout})
+      warn FAILED TO GET IBM CLOUD CASE ${case}
+    else
+      cat ${stdout} >>${json}
+    fi
+  fi
+}
+
+adduserstocases() {
+  emails=$1
+  for email in ${emails}
+  do
+    addusertocases ${email}
+  done
+}
+
+getparameters $*
+
+gettoken
+
+if [ $(echo ${caseParameters} | wc -w) -eq 0 ]
 then
-  error $(cat ${stdout})
-  error $(cat ${stderr})
-  error ERROR GETTING IBM CLOUD IDENTITY TOKEN
-  exit 1
-fi
-token=$(cat ${stdout} | grep -o '"access_token":"[^"]*' | grep -o '[^"]*$')
-if [ "${token}" = "" ]
-then
-  error $(cat ${stdout})
-  error FAILED TO GET IBM CLOUD IDENTITY TOKEN 
-  exit 1
+  getallcases
+else
+  getcases ${caseParameters} 
 fi
 
-info GET IBM CLOUD CASES
-curl -s -X GET "https://support-center.cloud.ibm.com/case-management/v1/cases?fields=number,watchlist&status=new,in_progress,waiting_on_client,resolution_provided" -H Authorization:${token} 1>${cases_json} 2>${stderr}
-if [ $? -ne 0 ] 
+if [ $(echo ${emailParameters} | wc -w) -eq 0 ]
 then
-  error $(cat ${cases_json})
-  error $(cat ${stderr})
-  error ERROR GETTING IBM CLOUD CASES
-  exit 1
-fi
-
-cat ${stdout} | grep '"number":' >/dev/null
-if  [ $? -ne 0 ]
-then
-  debug $(cat ${stdout})
-  warn NO IBM CLOUD CASES OPENED
-fi
-
-for email in ${emails}
-do
-  addusertocases ${email}
-done
+  adduserstocases "${emails}"
+else
+  adduserstocases "${emailParameters}"
+fi 
--- app/server.js
+++ app/server.js
@@ -1,21 +1,44 @@
 const HTTP = require('node:http');
+const URL = require('node:url');
 const { exec } = require('child_process');
 
 HTTP.createServer(function (request, response) {
 	
-    console.log("EXECUTE");
-   
-	response.writeHead(200, {'Content-Type': 'text/plain'});
+	var url = URL.parse(JSON.stringify(request.url),true)
+    console.log(url)
 
-    exec("./addUserToCaseWatchlist.sh", (error, stdout, stderr)=> {
+    var command = "."+request.url.substring(0,request.url.indexOf('?'))
+    if(url.query.apikey!=undefined) {
+        command = command.concat(" --apikey ",url.query.apikey)
+    }
+    if(url.query.email!=undefined) {
+        if(Array.isArray(url.query.email)) {
+            for (index in url.query.email) {
+                command = command.concat(" --email ",url.query.email[index])
+            }
+        } else command = command.concat(" --email ",url.query.email)
+    }
+    if(url.query.case!=undefined) {
+        if(Array.isArray(url.query.case)) {
+            for (index in url.query.case) {
+                command = command.concat(" --case ",url.query.case[index])
+            }
+        } else command = command.concat(" --case ",url.query.case)
+    }
+
+    console.log("EXECUTE "+command);
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    exec(command, (error, stdout, stderr)=> {
         if (error) {
             console.log(`error: ${error.message}`);
         }
         if (stderr) {
             console.log(`stderr: ${stderr}`);
         } 
-        console.log(`stdout: ${stdout}`);
-        response.write(`stdout: ${stdout}`);
+        console.log(`${stdout}`);
+        response.write(`${stdout}`);
         response.end()
     });
 }).listen(8080);
